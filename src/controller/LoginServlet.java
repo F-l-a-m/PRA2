1f8ea92d715d7abd9e8e05ab2510d4198b38c357
F-l-a-m <derlowe18@gmail.com>
2014-03-19 17:38:37 +0200

    Servlet

--- src/controller/LoginServlet.java
+++ src/controller/LoginServlet.java
@@ -11,9 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
  
-/**
- * Servlet implementation class LoginServlet
- */
 @WebServlet(
         description = "Login Servlet",
         urlPatterns = { "/LoginServlet" },
@@ -23,8 +20,7 @@ import javax.servlet.http.HttpServletResponse;
         })
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-        
-     
+    
     public void init() throws ServletException {
         //we can create DB connection resource here and set it to Servlet context
         if(getServletContext().getInitParameter("dbURL").equals("jdbc:mysql://localhost/mysql_db") &&
@@ -33,9 +29,9 @@ public class LoginServlet extends HttpServlet {
         getServletContext().setAttribute("DB_Success", "True");
         else throw new ServletException("DB Connection error");
     }
- 
      
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request,
+    		HttpServletResponse response) throws ServletException, IOException {
  
         //get request parameters for userID and password
         String user = request.getParameter("login");
@@ -49,14 +45,12 @@ public class LoginServlet extends HttpServlet {
          
         if(userID.equals(user) && password.equals(pwd)){
             response.sendRedirect("index-auth.jsp");
-        }else{
+        }
+        else{
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.jsp");
             PrintWriter out= response.getWriter();
             out.println("<font color=red>Either user name or password is wrong.</font>");
             rd.include(request, response);
-             
         }
-         
     }
- 
 }
